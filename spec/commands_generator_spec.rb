a3675ab50b26a23d70414cc406d8df934890f1d9
Jerome Lacoste <jerome.lacoste@gmail.com>
2015-08-13 11:48:33 +0200

    Make sure invalid configurations are detected

---
+++ spec/commands_generator_spec.rb
@@ -0,0 +1,9 @@
+require "gym/commands_generator"
+
+describe Gym do
+  describe Gym::CommandsGenerator do
+    it "validates available_options" do
+      # this runs the initialization code in the commands generator and detects invalid options
+    end
+  end
+end
